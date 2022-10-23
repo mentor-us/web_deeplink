deb56ebbe525029ae58b129086cfc99bd4bf84ff
tramhuuducvn <tramhuuduc.vn@gmail.com>
2022-10-23 21:22:46 +0700

    saved change!

--- src/pages/PersonalExercises/BTCN3/index.tsx
+++ src/pages/PersonalExercises/BTCN3/index.tsx
@@ -33,6 +33,7 @@ export default function BTCN3() {
     // Side effect
     useEffect(()=>{
         if(loading){
+            setMemeList([]);
             getMemeList();
         }
     }, [loading])
